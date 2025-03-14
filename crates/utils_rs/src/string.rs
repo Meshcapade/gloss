45ab998392488979a8c381cee3aaadcdf6a47669
Aman Shenoy <aman@meshcapade.com>
2025-03-14 11:11:32 +0530

    Remove gloss-memory and rename utils-rs

--- crates/utils_rs/src/string.rs
+++
@@ -1,5 +0,0 @@
-//formats f32 into string with specified precision
-// https://stackoverflow.com/a/61954174
-pub fn float2string(val: f32, precision: usize) -> String {
-    format!("{val:.precision$}")
-}
